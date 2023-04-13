bbb3e8700eeebefd20afd5fe0d98096d172fa547
Anandhu Anil <anandhu.anil@qburst.com>
2023-04-13 11:35:14 +0530

    Project setup and initializations.

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+
+// Boot express
+const app: Application = express();
+const port = 3000;
+
+// Application routing
+app.use("/", (req: Request, res: Response) => {
+  res.status(200).send({data: "Hello World"});
+});
+
+// Start server
+app.listen(port, () => console.log(`Server is listening on port ${port}!`));
