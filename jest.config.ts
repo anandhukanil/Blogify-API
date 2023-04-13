8a897d875d73615cf32cf1e39f2c1c10f559e988
Anandhu Anil <anandhu.anil@qburst.com>
2023-04-13 11:57:03 +0530

    Added jest and its configurations

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  roots: [
+    "<rootDir>/src"
+  ],
+  testEnvironment: "node",
+  preset: "ts-jest"
+};
